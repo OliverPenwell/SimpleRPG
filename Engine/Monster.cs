e2ee0705d6f90baed43d57058330fc66d9c7f5e9
Oliver Penwell <oliverjpen@protonmail.ch>
2019-06-18 14:49:45 +0100

    Added the rest of the required classes to Engine.

---
+++ Engine/Monster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    class Monster
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int CurrentHitPoint { get; set; }
+        public int MaximumDamage { get; set; }
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get; set; }
+    }
+}
